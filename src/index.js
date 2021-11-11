c7075a958f44210f4d68eef52ea559fecdd2413c
Alexey Sapr <alexeysapr@gmail.com>
2021-11-11 10:49:35 +0200

    Add axois

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import './sass/main.scss';
+import axios from 'axios';
 
-function getFotos() {
-  return fetch('https://api.pexels.com/v1/search?query=nature&per_page=10', {
-    method: 'GET',
-    headers: {
-      Authorization: '563492ad6f91700001000001926c4fbb77504078a21e31a37ce51bbd',
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.photos);
-      return data.photos;
-    });
+const AUTH_KEY = '563492ad6f91700001000001926c4fbb77504078a21e31a37ce51bbd';
+
+axios.defaults.baseURL = 'https://api.pexels.com/v1/search?query=nature&per_page=10';
+axios.defaults.headers.common['Authorization'] = AUTH_KEY;
+
+async function getFotos() {
+  try {
+    const response = await axios.get();
+    console.log(response.data.photos);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+// getFotos();
+
+// function getFotos() {
+//   return fetch('https://api.pexels.com/v1/search?query=nature&per_page=10', {
+//     method: 'GET',
+//     headers: {
+//       Authorization: '563492ad6f91700001000001926c4fbb77504078a21e31a37ce51bbd',
+//     },
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data.photos);
+//       return data.photos;
+//     });
+// }
 
 // getFotos();
 
